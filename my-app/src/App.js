3fe707cf5018e913f47ff8b6ee5e8cf09884d0f2
Robby Kloss <robk113355@gmail.com>
2025-05-16 12:36:29 -0500

    set up actual app

---
+++ my-app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+//git remote add origin https://github.com/robKloss33/robKloss33.github.io.git
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
